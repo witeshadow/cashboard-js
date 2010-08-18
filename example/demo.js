f138bfab9591fe19535d8c4309e0b8629bd8156d
Seth Banks <stb@subimage.com>
2010-08-18 15:32:22 -0700

    Demo properly lists items to HTML page.
    Now you don't have to inspect the JS console to see what you're getting
    back.
    
    Easy to see what's in an account if you have an API key.

--- example/demo.js
+++ example/demo.js
@@ -1,80 +1,9 @@
 var DEMO = {
-  // Cashboard connection
-  cb_conn: null,
+  cb_conn: null, // Cashboard connection
   auth_cookie_name: 'demo-credentials',
-  
-  // Run on page load
-  init: function() {
-    // Try to set auth credentials from cookie
-    var auth_cookie = COOKIES.get(DEMO.auth_cookie_name);
-    if (auth_cookie !== null) {
-      var auth_vals = JSON.parse(auth_cookie);
-      document.getElementById('subdomain').value = auth_vals['subdomain'];
-      document.getElementById('api_key').value = auth_vals['api_key']
-    }
-  },
+    
+  // STUFF THAT INTERACTS WITH THE API ----------------------------------------
 
-  // Grabs authentication credentials from HTML form and creates
-  // new Cashboard API connection.
-  //
-  // Allows us to change credentials in the HTML page multiple times
-  // for testing purposes.
-  authenticate: function() {
-    var subdomain = document.getElementById('subdomain').value;
-    var api_key = document.getElementById('api_key').value;
-    // Store credentials for page load refreshes.
-    COOKIES.set(
-      DEMO.auth_cookie_name, 
-      JSON.stringify({
-        subdomain: subdomain,
-        api_key: api_key
-      }),
-      COOKIES.getExpDate(90,0,0)
-    );
-    CASHBOARD.authenticate(subdomain, api_key);
-  },
-  
-  // Shortcut to grab display reference
-  display: function(content) {
-    var area = document.getElementById('display_area');
-    area.innerHTML = content;
-  },
-  // Quick and dirty way to display a Cashboard obj in the HTML.
-  // You should probably be using the DOM to make stuff, but I'm
-  // going for speed here.
-  display_cb_object: function(cb_obj) {
-    var html = "";
-    for(prop in cb_obj._data) {
-      html = html + "<b>"+prop+"</b> - ";
-      html = html + cb_obj._data[prop];
-      html = html + "<br/>";
-    }
-    DEMO.display(html);
-  },
-  
-  // Simply shows a loading indicator graphic
-  show_loading: function() {
-    var loading = document.getElementById('loading_message');
-    loading.style.display = 'block';
-    var buttons = document.getElementById('buttons');
-    buttons.style.display = 'none';
-  },
-  hide_loading: function() {
-    var loading = document.getElementById('loading_message');
-    loading.style.display = 'none';
-    var buttons = document.getElementById('buttons');
-    buttons.style.display = 'block';
-  },
-  
-  // Simple way to display errors on screen instead of logging
-  // to the console or alerting (default).
-  //
-  // See Cashboard.callback_failure as a template for overriding.
-  display_error: function(code, message) {
-    DEMO.hide_loading();
-    DEMO.display("Cashboard API Error " + code + ": " + message);
-  },
-  
   // Fetches account properties from API, then displays.
   get_account: function() {
     this.authenticate();
@@ -84,8 +13,7 @@ var DEMO = {
       // Will be passed a typecasted CASHBOARD.Account object.
       onSuccess: function(cb_account) {
         DEMO.hide_loading();
-        console.log(cb_account);
-        DEMO.display("Account retreived, please see the debugger console");
+        DEMO.display_cb_collection('Account', [cb_account]);
       }
     });
   },
@@ -98,8 +26,7 @@ var DEMO = {
       // Will be passed an array of typecasted CASHBOARD.Estimate objects.
       onSuccess: function(cb_estimates) {
         DEMO.hide_loading();
-        console.log(cb_estimates);
-        DEMO.display("Estimates retreived, please see the debugger console");
+        DEMO.display_cb_collection('Estimate', cb_estimates);
       }
     });
   },
@@ -112,22 +39,20 @@ var DEMO = {
       // Will be passed an array of typecasted CASHBOARD.Project objects.
       onSuccess: function(cb_projects) {
         DEMO.hide_loading();
-        console.log(cb_projects);
-        DEMO.display("Projects retreived, please see the debugger console");
+        DEMO.display_cb_collection('Project', cb_projects);
       }
     });
   },
   
-  get_tasks: function() {
+  get_line_items: function() {
     this.authenticate();
-    CASHBOARD.tasks.list({
+    CASHBOARD.line_items.list({
       onLoading: DEMO.show_loading,
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.LineItem objects.
       onSuccess: function(cb_line_items) {
         DEMO.hide_loading();
-        console.log(cb_line_items);
-        DEMO.display("Tasks retreived, please see the debugger console");
+        DEMO.display_cb_collection('Line Item', cb_line_items);
       }
     });
   },
@@ -140,8 +65,20 @@ var DEMO = {
       // Will be passed an array of typecasted CASHBOARD.Invoice objects.
       onSuccess: function(cb_invoices) {
         DEMO.hide_loading();
-        console.log(cb_invoices);
-        DEMO.display("Invoices retreived, please see the debugger console");
+        DEMO.display_cb_collection('Invoice', cb_invoices);
+      }
+    });
+  },
+  
+  get_payments: function() {
+    this.authenticate();
+    CASHBOARD.payments.list({
+      onLoading: DEMO.show_loading,
+      onFailure: DEMO.display_error,
+      // Will be passed an array of typecasted CASHBOARD.Payment objects.
+      onSuccess: function(cb_payments) {
+        DEMO.hide_loading();
+        DEMO.display_cb_collection('Payment', cb_payments);
       }
     });
   },
@@ -175,7 +112,98 @@ var DEMO = {
   // 
   // destroy_invoice: function() {
   //   my_invoice.destroy();
-  // }
+  // },
+  
+
+  // UTILITY METHODS ----------------------------------------------------------
+
+  // Grabs authentication credentials from HTML form and creates
+  // new Cashboard API connection.
+  //
+  // Allows us to change credentials in the HTML page multiple times
+  // for testing purposes.
+  authenticate: function() {
+    var subdomain = document.getElementById('subdomain').value;
+    var api_key = document.getElementById('api_key').value;
+    // Store credentials for page load refreshes.
+    COOKIES.set(
+      DEMO.auth_cookie_name, 
+      JSON.stringify({
+        subdomain: subdomain,
+        api_key: api_key
+      }),
+      COOKIES.getExpDate(90,0,0)
+    );
+    CASHBOARD.authenticate(subdomain, api_key);
+  },
+  
+  // Shortcut to grab display reference
+  display: function(content) {
+    var area = document.getElementById('display_area');
+    area.innerHTML = content;
+  },
+  
+  // Simple way to display errors on screen instead of logging
+  // to the console or alerting (default).
+  //
+  // See Cashboard.callback_failure as a template for overriding.
+  display_error: function(code, message) {
+    DEMO.hide_loading();
+    DEMO.display("Cashboard API Error " + code + ": " + message);
+  },
+  
+  // Displays a collection of Cashboard objects on the page.
+  display_cb_collection: function(item_type, objs) {
+    console.log(item_type + "...");
+    console.log(objs);
+    var html = "<h1>"+objs.length+" "+item_type+"(s)</h1>";
+    for (var i=0; i<objs.length; i++) {
+      html += DEMO.get_html_for_cb_object(item_type, objs[i]);
+    }
+    DEMO.display(html);
+  },
+
+  // Quick and dirty way to display a Cashboard obj in the HTML.
+  // You should probably be using the DOM to make stuff, but I'm
+  // going for speed of writing here.  
+  get_html_for_cb_object: function(obj_type, cb_obj) {
+    
+    var html = "<h2>"+ obj_type +" "+ (cb_obj['id'] || '') +"</h2>";
+    html += "<table class='properties'>";
+    for(prop in cb_obj._data) {
+      html += "<tr><th>"+prop+"</th><td>";
+      html += cb_obj._data[prop];
+      html += "</td></tr>";
+    }
+    html += "</table>"
+    return html;    
+  },
+  
+  // Simply shows a loading indicator graphic
+  show_loading: function() {
+    var loading = document.getElementById('loading_message');
+    loading.style.display = 'block';
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'none';
+    DEMO.display('');
+  },
+  hide_loading: function() {
+    var loading = document.getElementById('loading_message');
+    loading.style.display = 'none';
+    var buttons = document.getElementById('buttons');
+    buttons.style.display = 'block';
+  },
+  
+  // Run on page load
+  init: function() {
+    // Try to set auth credentials from cookie
+    var auth_cookie = COOKIES.get(DEMO.auth_cookie_name);
+    if (auth_cookie !== null) {
+      var auth_vals = JSON.parse(auth_cookie);
+      document.getElementById('subdomain').value = auth_vals['subdomain'];
+      document.getElementById('api_key').value = auth_vals['api_key']
+    }
+  }
 };
 
 addEvent(window, 'load', DEMO.init);

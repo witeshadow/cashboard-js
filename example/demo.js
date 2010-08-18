113bdab8a8a4f68541d99bb2be767d5bdb65326c
Seth Banks <stb@subimage.com>
2010-08-18 00:42:41 -0700

    Not ready for prime-time yet.
    Got listing of resources down, but still debating the merits of the
    object organization I've laid out. Looking for a more smooth way to
    instantiate objects after we've listed them.
    
    See "get_account" in demo.js for an idea of what's going on now. Don't
    like having to pass the connection around to all the objects. Thinking
    about having the connection be a global we can reference from
    everywhere.
    
    This is just a starting point to continue from. Don't recommend anyone
    uses this in production just yet.

---
+++ example/demo.js
@@ -0,0 +1,93 @@
+var DEMO = {
+  // Cashboard connection
+  cb_conn: null,
+  
+
+  // Grabs authentication credentials from HTML form and creates
+  // new Cashboard API connection.
+  //
+  // Allows us to change credentials in the HTML page multiple times
+  // for testing purposes.
+  authenticate: function() {
+    var subdomain = document.getElementById('subdomain').value;
+    var api_key = document.getElementById('api_key').value;
+    this.cb_conn = new Cashboard(subdomain, api_key);
+  },
+  // Shortcut to grab display reference
+  display: function(content) {
+    var area = document.getElementById('display_area');
+    area.innerHTML = content;
+  },
+  
+  // Simple way to display errors on screen instead of logging
+  // to the console or alerting (default).
+  //
+  // See Cashboard.callback_failure as a template for overriding.
+  display_error: function(code, message) {
+    this.display("Cashboard API Error " + code + ": " + message);
+  },
+  
+  // Notice that we're using the global reference DEMO.cb_conn
+  // to pass to the new CashboardObject.
+  get_account: function() {
+    this.authenticate();
+    this.cb_conn.account.list({
+      onFailure: this.display_error,
+      onSuccess: function(json_obj) {
+        acct = new CashboardObject(DEMO.cb_conn, json_obj);
+        console.log(acct);
+      }
+    });
+  },
+    
+  get_estimates: function() {
+    this.authenticate();
+    this.cb_conn.estimates.list();
+  },
+  
+  get_projects: function() {
+    this.authenticate();
+    this.cb_conn.projects.list();
+  },
+  
+  get_tasks: function() {
+    this.authenticate();
+    this.cb_conn.tasks.list();
+  },
+  
+  get_invoices: function() {
+    this.authenticate();
+    this.cb_conn.invoices.list();
+  },
+  
+  // create_new_invoice: function() {
+  //   this.cb_conn.invoices.create(
+  //     {
+  //       assigned_id: 'INV-00010',
+  //       client_id: 12345,
+  //       client_type: 'Company',
+  //       due_date: "1/10/2012"
+  //     },
+  //     {
+  //       onSuccess: displayStatusMessage,
+  //       onFailure: displayStatusMessage
+  //     }
+  //   );
+  // },
+  // 
+  // update_invoice: function() {
+  //   my_invoice.update(
+  //     {
+  //       due_date: "1/15/2012"
+  //     },
+  //     {
+  //       onSuccess: doSomething,
+  //       onFailure: doSomethingElse
+  //     }
+  //   );    
+  // },
+  // 
+  // destroy_invoice: function() {
+  //   my_invoice.destroy();
+  // }
+};

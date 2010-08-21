367d69c22a2f18faf5b7fac3998feb1eeec8db05
Seth Banks <stb@subimage.com>
2010-08-20 22:51:37 -0700

    Added example of deleting to demo.
    Also cleaned up some code when parsing regular text responses in the
    API.

--- example/demo.js
+++ example/demo.js
@@ -1,6 +1,7 @@
 var DEMO = {
   cb_conn: null, // Cashboard connection
   auth_cookie_name: 'demo-credentials',
+  cached_items: {},
     
   // STUFF THAT INTERACTS WITH THE API ----------------------------------------
 
@@ -12,7 +13,6 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed a typecasted CASHBOARD.Account object.
       onSuccess: function(cb_account) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection('Account', [cb_account]);
       }
     });
@@ -25,7 +25,6 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.Estimate objects.
       onSuccess: function(cb_estimates) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection('Estimate', cb_estimates);
       }
     });
@@ -45,7 +44,6 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.Project objects.
       onSuccess: function(cb_projects) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection('Project', cb_projects);
       }
     });
@@ -70,7 +68,6 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.LineItem objects.
       onSuccess: function(cb_line_items) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection(type, cb_line_items);
       }
     });
@@ -83,7 +80,6 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.Invoice objects.
       onSuccess: function(cb_invoices) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection('Invoice', cb_invoices);
       }
     });
@@ -96,43 +92,10 @@ var DEMO = {
       onFailure: DEMO.display_error,
       // Will be passed an array of typecasted CASHBOARD.Payment objects.
       onSuccess: function(cb_payments) {
-        DEMO.hide_loading();
         DEMO.display_cb_collection('Payment', cb_payments);
       }
     });
   },
-  
-  // create_new_invoice: function() {
-  //   CASHBOARD.invoices.create(
-  //     {
-  //       assigned_id: 'INV-00010',
-  //       client_id: 12345,
-  //       client_type: 'Company',
-  //       due_date: "1/10/2012"
-  //     },
-  //     {
-  //       onSuccess: displayStatusMessage,
-  //       onFailure: displayStatusMessage
-  //     }
-  //   );
-  // },
-  // 
-  // update_invoice: function() {
-  //   my_invoice.update(
-  //     {
-  //       due_date: "1/15/2012"
-  //     },
-  //     {
-  //       onSuccess: doSomething,
-  //       onFailure: doSomethingElse
-  //     }
-  //   );    
-  // },
-  // 
-  // destroy_invoice: function() {
-  //   my_invoice.destroy();
-  // },
-  
 
   // UTILITY METHODS ----------------------------------------------------------
 
@@ -158,6 +121,7 @@ var DEMO = {
   
   // Shortcut to grab display reference
   display: function(content) {
+    DEMO.hide_loading();
     var area = document.getElementById('display_area');
     area.innerHTML = content;
   },
@@ -167,12 +131,14 @@ var DEMO = {
   //
   // See Cashboard.callback_failure as a template for overriding.
   display_error: function(code, message) {
-    DEMO.hide_loading();
     DEMO.display("Cashboard API Error " + code + ": " + message);
   },
   
   // Displays a collection of Cashboard objects on the page.
   display_cb_collection: function(item_type, objs) {
+    // Cache items for lookup later (when deleting / updating)
+    DEMO.cached_items[item_type] = objs;
+    
     console.log(item_type + "...");
     console.log(objs);
     var html = "<h1>"+objs.length+" "+item_type+"(s)</h1>";
@@ -182,19 +148,51 @@ var DEMO = {
     DEMO.display(html);
   },
 
+
+  destroy: function(obj_type, obj_id) {
+    var obj = null;
+    var collection = DEMO.cached_items[obj_type];
+    if (collection == null || collection.size == 0) {
+      throw "No items in collection for " + obj_type;
+    }
+    // Find item in cached collection that was populated when displaying.
+    for (var i=0; i<collection.length; i++) {
+      if (obj_id === collection[i].id) {
+        obj = collection[i];
+        break;;
+      }
+    }
+    if (obj == null) {
+      throw "Object not found in cached list";
+    }
+    console.log(obj);
+    if (confirm("Do you really want to delete this item?")) {
+      obj.destroy({
+        onLoading: DEMO.show_loading,
+        onFailure: DEMO.display_error,
+        onSuccess: function(response_thing) {
+          DEMO.display(obj_type + " #" + obj_id + " was deleted successfully.");
+        }
+      })
+    }
+  },
+
+
   // Quick and dirty way to display a Cashboard obj in the HTML.
   // You should probably be using the DOM to make stuff, but I'm
   // going for speed of writing here.  
   get_html_for_cb_object: function(obj_type, cb_obj) {
     
-    var html = "<h2>"+ obj_type +" "+ (cb_obj['id'] || '') +"</h2>";
+    var html = "<h2>"+ obj_type +" "+ (cb_obj['id'] || '');
+    html += "<a href='#' onclick='DEMO.destroy(\""+ obj_type +"\", "+ cb_obj.id +");return false;'>Delete</a>";
+    html += "</h2>";
     html += "<table class='properties'>";
     for(prop in cb_obj._data) {
       html += "<tr><th>"+prop+"</th><td>";
       html += cb_obj._data[prop];
       html += "</td></tr>";
     }
-    html += "</table>"
+    html += "</table>";
     return html;    
   },
   
